820266e2abd35c8959c88d7890947e633fc05e2a
leonardolira14 <leon_lira_@hotmail.com>
2019-06-05 22:46:33 -0500

    asd

--- admin/application/config/config.php
+++ admin/application/config/config.php
@@ -24,6 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 $config['base_url'] = 'https://agenciapegasus.com.mx/restaurante/admin/';
+//$config['base_url'] = 'http://localhost/restaurante/admin/';
 
 /*
 |--------------------------------------------------------------------------
